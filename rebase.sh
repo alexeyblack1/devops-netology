7d6822357595b3efb5c1374492039e5f2e4f8e8e
AlexeyBlack <alexeyblack1@gmail.com>
2021-05-18 15:31:14 +0300

    change rebase.sh in git-rebase

--- rebase.sh
+++ rebase.sh
@@ -3,8 +3,20 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
+
     echo "\$@ Parameter #$count = $param"
+
+
+
+=======
+    echo "Next parameter: $param"
+>>>>>>> 6754e07... git-rebase 2
     count=$(( $count + 1 ))
 done
 
-echo "====="
+
+
+
+
+
